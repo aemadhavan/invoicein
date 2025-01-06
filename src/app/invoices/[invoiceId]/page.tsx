85c439790c4e226ebe76d595d4385dd0f0ee94e2
Madhavan <enmadhavan@gmail.com>
2025-01-06 14:00:39 +1100

    Deployment Error Fix

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,4 +1,3 @@
-
 import { notFound } from "next/navigation";
 import { getInvoice } from "@/app/actions";
 import Invoice from "./invoice";
@@ -8,20 +7,20 @@ type InvoiceWithCustomer = typeof Invoices.$inferSelect & {
   customer: typeof Customers.$inferSelect;
 };
 
-export default async function InvoicePage({params}:{params: {invoiceId: string}}) {  
-  const awaitedParams = await params;    
-  const invoiceId = Number.parseInt(awaitedParams.invoiceId);
-  if(isNaN(invoiceId)) {
+export default async function InvoicePage({
+  params,
+}: {
+  params: { invoiceId: string };
+}) {
+  const invoiceId = Number.parseInt(params.invoiceId);
+  if (isNaN(invoiceId)) {
     throw new Error('Invalid invoice ID');
   }
-  const invoice =  await getInvoice(invoiceId);
 
-  if(!invoice) {
+  const invoice = await getInvoice(invoiceId);
+  if (!invoice) {
     notFound();
   }
-  
-  console.log(invoice);
-  return (
-    <Invoice invoice={invoice as InvoiceWithCustomer} />
-  );  
+
+  return <Invoice invoice={invoice as InvoiceWithCustomer} />;
 }
